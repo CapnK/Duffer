1995d4c971fb499bad13a386d199be4755ac2827
Steve Downing <steven.downing@arup.com>
2014-01-26 22:55:48 +1100

    - Basic steps for MetaData

---
+++ Duffer.Tests/Steps/MetaDataSteps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Duffer.Tests.Steps
+{
+    [Binding]
+    public class MetaDataSteps
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        [Given(@"the metadata for ""(.*)"" has the following entries")]
+        public void GivenTheMetadataForHasTheFollowingEntries(string p0, Table table)
+        {
+            ScenarioContext.Current.Pending();
+
+        }
+
+        [Given(@"the metadata for ""(.*)"" has a RHAdobeMeta entry with the following values")]
+        public void GivenTheMetadataForHasARHAdobeMetaEntryWithTheFollowingValues(string p0, Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+    }
+}
